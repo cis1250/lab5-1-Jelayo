e878a6914287bf0c91c48659c0f9a61a455fa7a1
Jelayo <jelayo@uoguelph.ca>
2025-10-21 11:31:09 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python3
-
-# Fibonacci Sequence Exercise with functions
-# TODO: (Read detailed instructions in the Readme file)
 def ask_input():
-  fib = input("How many terms of the Fibonacci sequence would you like?")
-  if int(fib) <= 0:
-    while (fib <= 0):
-      print("Pick a number greater than 0")
-      input("How many terms of the Fibonacci sequence would you like?")
-   return fib
+    fib = input("How many terms of the Fibonacci sequence would you like? ")
+    if int(fib) <= 0:
+        while (fib <= 0):
+            print("Pick a number greater than 0")
+            input("How many terms of the Fibonacci sequence would you like?")
+    return fib
 
-  def return_fib():
-      x = 0
-      y = 1
-      z = 0
-      print("0")
-      for z in range (int(fib)):
-          x, y = y, x + y
-          if x >= int(fib): break
-          fib_list.append(x)  
-      return fib_list
+def return_fib(fib):
+    x = 0
+    y = 1
+    z = 0
+    fib_list = [0]
+    for z in range (int(fib)):
+        x, y = y, x + y
+        if x >= int(fib): break
+        fib_list.append(x)  
+    return fib_list
 
 def print_fib():
   fib = ask_input()
