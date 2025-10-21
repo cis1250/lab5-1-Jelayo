11ccb3f5e0f205715cb28387e6e5891aeab290a4
Jelayo <jelayo@uoguelph.ca>
2025-10-21 12:12:40 -0400

    word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -1,26 +1,51 @@
-#!/usr/bin/env python3
-
-# Word frequency exercise
-# TODO: (Read detailed instructions in the Readme file)
-
 import re
 
-#This is a function that checks if a text qualifies as a sentence. You do not need to modify this!
 def is_sentence(text):
-    # Check if the text is not empty and is a string
     if not isinstance(text, str) or not text.strip():
         return False
 
-    # Check for starting with a capital letter
     if not text[0].isupper():
         return False
 
-    # Check for ending punctuation
     if not re.search(r'[.!?]$', text):
         return False
 
-    # Check if it contains at least one word (non-whitespace characters)
     if not re.search(r'\w+', text):
         return False
 
     return True
+
+def get_sentence():
+    user_sentence = input("Enter a sentence: ")
+    return user_sentence
+
+def calculate_frequencies(user_sentence):
+        user_sentence = user_sentence.replace(",", "")
+        user_sentence = user_sentence.replace(".", "")
+        divided = user_sentence.lower().split()
+        return divided
+
+def sentence_check(user_sentence):
+    while (is_sentence(user_sentence) == False):
+        print("This does not meet the criteria for a sentence.")
+        user_sentence = input("Enter a sentence: ")
+    if (is_sentence(user_sentence) == True):
+            calculate_frequencies(user_sentence)
+
+def print_frequencies(divided):
+    unique = []
+    for word in divided:
+        if word not in unique:
+            unique.append(word)
+    for y in unique:
+        print(y.title(),"-->",  divided.count(y))
+
+
+def run():
+    text = get_sentence()
+    is_sentence(text)
+    sentence_check(text)
+    divided = calculate_frequencies(text)
+    print_frequencies(divided)
+
+run()
