635fd1e1b75686bcb8c587184721ea1a18714aeb
Jelayo <jelayo@uoguelph.ca>
2025-10-21 10:51:09 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,18 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+
+fib = input("How many terms of the Fibonacci sequence would you like?")
+if int(fib) <= 0:
+  while (fib <= 0):
+    print("Pick a number greater than 0")
+    input("How many terms of the Fibonacci sequence would you like?")
+else:
+    x = 0
+    y = 1
+    z = 0
+    print("0")
+    for z in range (int(fib)):
+        x, y = y, x + y
+        if x >= int(fib): break
+        print(x)
